b0644bc274acb4f778001199082575fa322a9fc8
Mohammad <mohammad.joudy@gmail.com>
2023-12-20 16:29:32 +0100

    test module created. notebook for developing parse by content created.

---
+++ test_funcs_module.py
@@ -0,0 +1,21 @@
+import pytest
+from funcs_module import main
+
+def test_main():
+    pdf_path = "/path/to/sample.pdf"  # Replace with the actual path to a sample PDF file
+    expected_output = [
+        {
+            'title': 'Chapter 1',
+            'pages': '1-5',
+            'text': ['Lorem ipsum dolor sit amet, consectetur adipiscing elit.', 'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium.'],
+            'images': [['image_1_1.png', 'image_1_2.png']]
+        },
+        {
+            'title': 'Chapter 2',
+            'pages': '6-10',
+            'text': ['Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit.', 'Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur.'],
+            'images': [['image_2_1.png', 'image_2_2.png', 'image_2_3.png']]
+        }
+    ]  # Replace with the expected output for the sample PDF file
+    
+    assert main(pdf_path) == expected_output
